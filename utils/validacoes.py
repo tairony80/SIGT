14d0a70861ed3744ddcbdf9488c843492a019155
tairony80 <tairony.df80@gmail.com>
2026-08-06 16:05:53 -0300

    Update validacoes.py

--- utils/validacoes.py
+++ utils/validacoes.py
@@ -1,7 +1,14 @@
 import re
 
+# ===================================================
+# VALIDAÇÃO DE E-MAIL
+# ===================================================
 
 def email_valido(email):
+    """
+    Verifica se o e-mail informado possui um formato válido.
+    Retorna True ou False.
+    """
 
     padrao = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
