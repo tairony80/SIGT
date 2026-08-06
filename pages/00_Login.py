c6ae6c9358628a05bf850b6e1e9361963b4dbdbe
tairony80 <tairony.df80@gmail.com>
2026-08-06 14:59:19 -0300

    Update 00_Login.py

--- pages/00_Login.py
+++ pages/00_Login.py
@@ -1,10 +1,39 @@
 import streamlit as st
 
+from database.models import buscar_usuario_login
+from database.seguranca import verificar_senha
+
 st.set_page_config(
     page_title="Login",
     page_icon="🔐"
 )
 
-st.title("🔐 Login")
+st.title("🔐 Login do SIGT")
 
-st.info("Tela de login em construção.")
+login = st.text_input("Usuário")
+
+senha = st.text_input(
+    "Senha",
+    type="password"
+)
+
+if st.button("Entrar"):
+
+    usuario = buscar_usuario_login(login)
+
+    if usuario is None:
+
+        st.error("Usuário não encontrado.")
+
+    else:
+
+        if verificar_senha(
+            senha,
+            usuario[3]
+        ):
+
+            st.success("Login realizado com sucesso!")
+
+        else:
+
+            st.error("Senha incorreta.")
