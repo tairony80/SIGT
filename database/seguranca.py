b6f41a35a12e0d48bf611c890cb7ba28ca02501f
tairony80 <tairony.df80@gmail.com>
2026-08-06 14:54:54 -0300

    Update seguranca.py

--- database/seguranca.py
+++ database/seguranca.py
@@ -7,3 +7,9 @@ def criptografar_senha(senha):
     ).hexdigest()
 
     return senha_hash
+
+def verificar_senha(senha_digitada, senha_banco):
+
+    senha_hash = criptografar_senha(senha_digitada)
+
+    return senha_hash == senha_banco
